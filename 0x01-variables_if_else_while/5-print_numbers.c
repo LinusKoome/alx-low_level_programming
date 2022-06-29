3fbb0dbba6f10ebd8490bc60a8824617aac308ba
Linus Koome <linuskoome1998@gmail.com>
2022-06-29 00:06:36 +0000

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main-the entry point
+ * Return:void
+ */
+
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers < 10)
+	{
+		printf("%d", numbers);
+		numbers++;
+	}
+	printf("\n");
+	return (0);
+}
