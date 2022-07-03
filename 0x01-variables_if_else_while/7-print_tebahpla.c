993033ee68637591d8fd4b075056ea4da490b2a1
Linus Koome <linuskoome1998@gmail.com>
2022-07-03 19:01:27 +0000

    Solve 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-the entry point
+ * Return:void
+ */
+
+int main(void)
+{
+	char a;
+
+		for (a = 'z'; a >= 'a'; a--)
+		{
+			putchar(a);
+		}
+
+		putchar('\n');
+
+		return (0);
+}
