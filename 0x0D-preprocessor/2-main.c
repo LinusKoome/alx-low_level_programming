e34220cb636e0011b9749b7345bdf4bf803d5eb1
Linus Koome <linuskoome1998@gmail.com>
2022-07-05 00:14:58 +0000

    Answer all and add readme file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-the entry point
+ * Return:void
+ */
+
+int main(void)
+{
+	printf("%\n",__FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+The project deals with the preprocessor
